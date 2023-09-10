5899e07b3ec89a5e8c7ccae38e2fe4c796296699
yoonsseo <yellisa2000@gmail.com>
2023-09-10 12:51:40 +0900

    [BOJ_2018] 수들의 합5

--- yoonsseo/BOJ_2018.py
+++ yoonsseo/BOJ_2018.py
@@ -1,14 +1,15 @@
 N = int(input())
-start = end = sum = cnt = 1
-while end != N:
-    if sum == N:
+start = end = 1
+sum = cnt = 0
+while start <= N :
+    if sum == N :
         cnt += 1
-        end += 1
-        sum += end
-    elif sum > N:
         sum -= start
         start += 1
-    else:
-        end += 1
+    elif sum > N :
+        sum -= start
+        start += 1
+    else : # sum < N
         sum += end
+        end += 1
 print(cnt)
