c789f9831ea65312d6ecfad407dd4ab3d59e76f6
yoonsseo <yellisa2000@gmail.com>
2023-10-08 21:31:38 +0900

    [BOJ] 1010: 피보나치 수열

--- yoonsseo/BOJ_1010.py
+++ yoonsseo/BOJ_1010.py
@@ -1,11 +1,16 @@
-dp = [[0] * 31 for _ in range(31)]
-for i in range(31):
-    dp[i][i] = 1
-    dp[1][i] = i
-    for j in range(i+1, 31):
-        dp[i][j] = dp[i][j-1] + dp[i-1][j-1]
+zero = [0] * 41
+zero[0] = 1
+zero[2] = 1
+
+one = [0] * 41
+one[1] = 1
+one[2] = 1
+
+for i in range (3, 41) :
+    zero[i] = zero[i-2] + zero[i-1]
+    one[i] = one[i-2] + one[i-1]
 
 T = int(input())
-for _ in range(T):
-    N, M = map(int, input().split())
-    print(dp[N][M])
+for _ in range (T) :
+    N = int(input())
+    print("%d %d" %(zero[N], one[N]))
